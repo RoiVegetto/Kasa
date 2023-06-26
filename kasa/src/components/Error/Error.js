15ed80d0f24dfa827434b53bfac50f7c767d9419
Benjamin Corroyez <benjamincorroyez@hotmail.fr>
2023-06-26 17:27:18 +0200

    refactor : Correction des erreurs en console Derniers ajustement en css

--- kasa/src/components/Error/Error.js
+++ kasa/src/components/Error/Error.js
@@ -3,17 +3,19 @@ import './Error.css';
 import { Link } from 'react-router-dom';
 
 /**
- * Error404 renvoi la page d'erreur 
- * @param {*} props 
- * @returns 
+ * Error404 renvoi la page d'erreur
+ * @param {*} props
+ * @returns
  */
 
 function Error404(props) {
   return (
     <section className="error-section">
       <h1>{props.h1}</h1>
-      <p>{props.p}</p>
-      <p><Link to="/">{props.href}</Link></p>
+      <p className="description-error">{props.p}</p>
+      <p className="return">
+        <Link to="/">{props.href}</Link>
+      </p>
     </section>
   );
 }
